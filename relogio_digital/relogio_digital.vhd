81a098fe3f6898fcb10d50497f3333c005960ae6
William Vernaschi <williamdavi13@gmail.com>
2022-11-12 12:37:57 -0300

    relogio completo funcionando (aparentemente)

--- relogio_digital/relogio_digital.vhd
+++ relogio_digital/relogio_digital.vhd
@@ -5,7 +5,10 @@ ENTITY relogio_digital IS
 	PORT(CLEAR: IN BIT;
 	     CLOCK: IN STD_LOGIC;
 	       SEG0: BUFFER BIT_VECTOR(3 DOWNTO 0);
-	       SEG1: BUFFER BIT_VECTOR(2 DOWNTO 0));
+	       SEG1: BUFFER BIT_VECTOR(2 DOWNTO 0);
+	       MIN0: BUFFER BIT_VECTOR(3 DOWNTO 0);
+	       MIN1: BUFFER BIT_VECTOR(2 DOWNTO 0));
+	       
 END relogio_digital;
 
  
@@ -31,12 +34,20 @@ COMPONENT cont_mod6 IS
 	    OUTPUT: OUT BIT_VECTOR(2 DOWNTO 0));
 END COMPONENT;
 
-SIGNAL Q, QM: BIT_VECTOR(3 DOWNTO 0) := "0000";
-SIGNAL QB, QBM: BIT_VECTOR(3 DOWNTO 0) := "1111";
-SIGNAL SEG1_CLK: STD_LOGIC;
+SIGNAL SEG1_CLK, MIN0_CLK, MIN1_CLK: STD_LOGIC;
 
 BEGIN
 	contador_segundos0: cont_mod10 PORT MAP(CLEAR, CLOCK, SEG0);
+	
 	SEG1_CLK <= to_std_logic(NOT(NOT SEG0(0) AND NOT SEG0(1) AND NOT SEG0(2) AND NOT SEG0(3)));
-	contador_segundos1:  cont_mod6  PORT MAP(CLEAR, SEG1_CLK, SEG1);
+	
+	contador_segundos1:  cont_mod6 PORT MAP(CLEAR, SEG1_CLK, SEG1);
+	
+	MIN0_CLK <= to_std_logic(NOT(NOT SEG1(0) AND NOT SEG1(1) AND NOT SEG1(2)));
+	
+	contador_minutos0: cont_mod10 PORT MAP(CLEAR, MIN0_CLK, MIN0);
+	
+	MIN1_CLK <= to_std_logic(NOT(NOT MIN0(0) AND NOT MIN0(1) AND NOT MIN0(2) AND NOT MIN0(3)));
+	
+	contador_minutos1: cont_mod6 PORT MAP(CLEAR, MIN1_CLK, MIN1);
 END arch;
